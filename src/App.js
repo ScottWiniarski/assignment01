05bb2bbcfbb43011afc1f7439813ee3e5f8b7e78
Scott Winiarski <Scott1w1@hotmail.com>
2023-08-23 10:39:36 -0500

    Adjusted program to use array in App.js. * Removed local state from country.jsx * Moved mathematical logic into App.js through methods.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import Country from './components/country';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 class App extends Component {
+  state = {
+    countries: [
+      {id: 1, country: 'United States', gold: 5},
+      {id: 2, country: 'Canada', gold: 3},
+      {id: 3, country: 'England', gold: 4},
+      {id: 4, country: 'Spain', gold: 6},
+    ]
+  }
+
+  handleAddition = (countryId) => {
+    console.log(countryId);
+    for(let i = 0; i<this.state.countries.length; i++){
+      if(this.state.countries[i].id === countryId){
+        let countries = [...this.state.countries];
+        countries[i].gold++;
+        this.setState({countries:countries});
+        break;
+      }
+    }
+  }
+
+  handleSubtraction = (countryId) => {
+    console.log(countryId);
+
+    //this.props.country.gold > 0 && this.setState({gold: this.props.country.gold - 1});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Awards
         </header>
-        <Country />       
+        { this.state.countries.map(country =>
+        <Country
+          key={country.id}
+          country = {country}
+          addGold = {this.handleAddition}
+          minusGold = {this.handleSubtraction}
+          />)}      
       </div>
     );
   }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -3,45 +3,22 @@ import { Button, Card } from '@mui/material';
 import React, { Component } from 'react';
 
 class Country extends Component {
-    state = { 
-      name: 'America',
-      gold: 0,
-     }
-      // helper method
-    renderName(name) {
-      return (name === undefined || name === null ? 'Unknown' : name);
-    }
-    renderGold(gold){
-      return (gold === undefined || gold === null? 0 : gold);
-    }
-
-    increaseGold = () => {
-      // convert the function to an arrow function
-      // arrow functions inherit 'this' keyword
-      //console.log(this.state.gold);
-      console.log("Increasing Gold");
-      // the setState method is inherited from the base Component class
-      // when a component's state is altered, it is re-rendered asynchronously by react
-      this.setState({ gold: this.state.gold + 1 })
-    }
-    decreaseGold = () => {
-      console.log("Decreasing Gold");
-      this.state.gold > 0 && this.setState({gold: this.state.gold - 1});
-    }
+  
     render() { 
+      const {addGold, minusGold, country} = this.props;
+
       return (
         <Card variant="outlined">
         
-          <div className='Country' style={{name:this.renderName(this.state.name) }}>
-          {<span style={{ fontWeight: 'bold' }}>{this.state.name}</span>}
+          <div className='Country' style={{ name:(this.props.country) }}>
+          {<span style={{ fontWeight: 'bold' }}>{this.props.country.country}</span>}
             
           </div>
-          <div className='Country' style={{ gold:this.renderGold(this.state.gold) }}>
-            {"Gold Medals: " + this.state.gold + " "}
-            <button onClick={this.increaseGold}> + </button>
-            <Button variant='outlined' color='success' size='small' onClick={this.increaseGold}>+</Button>
+          <div className='Country' style={{ gold:this.props.country.gold }}>
+            {"Gold Medals: " + this.props.country.gold + " "}
+            <Button variant='outlined' color='success' size='small' onClick={ () => this.props.addGold(this.props.country.id) }>+</Button>
             {/* <Button variant='outlined' color='success' size='small'>{this.AddOutlinedIcon}</Button> */}
-            <Button variant='outlined' color='error' size='small' onClick={this.decreaseGold}>-</Button>
+            <Button  variant='outlined' color='error' size='small' onClick={this.decreaseGold}>-</Button>
             {/* <button onClick={this.decreaseGold}> - </button> */}
           </div>
          
